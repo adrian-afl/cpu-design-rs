0873ad786abd6e76ca4909d76d8bad3dda48291f
Adrian <4716150+adrian-afl@users.noreply.github.com>
2026-04-29 21:07:10 +0200

    the minicpu seems to work in iverilog now fine, reworked the addressing, also added a modified version of tang nano 20k serial example to later integrate

--- raw_verilog_stuff/minicpu/cpu.v
+++ raw_verilog_stuff/minicpu/cpu.v
@@ -1,76 +1,170 @@
 module cpu(
     input clk,
     input rst,
-    input [7:0] instr,
-    input [15:0] instr_data,
+
+    input [15:0] memory_readout,
+
+    output reg [15:0] memory_writeout,
+    output reg memory_write_enable,
+    output reg [15:0] address_bus = 0,
 
     output [15:0] out_reg_x,
     output [15:0] out_reg_y,
     output [15:0] out_reg_accu,
+    output [15:0] out_reg_pc,
+    output [15:0] out_reg_sp,
     output out_carry
 );
 
-`define INSTR_STX_DIRECT 8'h10
-`define INSTR_STY_DIRECT 8'h11
+`define INSTR_STX_DIRECT 16'h10
+`define INSTR_STY_DIRECT 16'h11
 
-`define INSTR_MEM_LX_DIRECT 8'h12
-`define INSTR_MEM_LY_DIRECT 8'h13
+`define INSTR_MEM_LX_DIRECT 16'h12
+`define INSTR_MEM_LY_DIRECT 16'h13
 
-`define INSTR_MEM_LX_INDIRECT 8'h14
-`define INSTR_MEM_LY_INDIRECT 8'h15
+`define INSTR_MEM_LX_INDIRECT 16'h14
+`define INSTR_MEM_LY_INDIRECT 16'h15
 
-`define INSTR_MEM_SX_DIRECT 8'h16
-`define INSTR_MEM_SY_DIRECT 8'h17
+`define INSTR_MEM_SX_DIRECT 16'h16
+`define INSTR_MEM_SY_DIRECT 16'h17
 
-`define INSTR_MEM_SX_INDIRECT 8'h18
-`define INSTR_MEM_SY_INDIRECT 8'h19
+`define INSTR_MEM_SX_INDIRECT 16'h18
+`define INSTR_MEM_SY_INDIRECT 16'h19
 
-`define INSTR_ADD 8'h20
-`define INSTR_SUB 8'h21
-`define INSTR_MUL 8'h22
-`define INSTR_DIV 8'h23
+`define INSTR_ADD 16'h20
+`define INSTR_SUB 16'h21
+`define INSTR_MUL 16'h22
+`define INSTR_DIV 16'h23
 
-`define INSTR_MOVE_X_ACCU 8'h30
-`define INSTR_MOVE_Y_ACCU 8'h31
-`define INSTR_MOVE_X_Y 8'h32
-`define INSTR_MOVE_Y_X 8'h33
-`define INSTR_MOVE_ACCU_X 8'h34
-`define INSTR_MOVE_ACCU_Y 8'h35
+`define INSTR_MOVE_X_ACCU 16'h30
+`define INSTR_MOVE_Y_ACCU 16'h31
+`define INSTR_MOVE_X_Y 16'h32
+`define INSTR_MOVE_Y_X 16'h33
+`define INSTR_MOVE_ACCU_X 16'h34
+`define INSTR_MOVE_ACCU_Y 16'h35
 
-    reg [15:0] ram [16384:0];
+    reg [8:0] current_state = 0;
+
+    reg [15:0] reg_pc = 0;
+    reg [15:0] reg_sp = 0;
+
+    reg [15:0] reg_instr;
+    reg [15:0] reg_instr_data;
 
     reg [15:0] reg_x;
     reg [15:0] reg_y;
     reg [16:0] reg_accu;
 
     always @(posedge clk) begin
-        case(instr)
-            `INSTR_STX_DIRECT:
-                reg_x <= instr_data;
-            `INSTR_STY_DIRECT:
-                reg_y <= instr_data;
-            `INSTR_MEM_LX_DIRECT:
-                reg_x <= ram[instr_data[15:0]];
-            `INSTR_MEM_LY_DIRECT:
-                reg_y <= ram[instr_data[15:0]];
-            `INSTR_MEM_SX_DIRECT:
-                ram[instr_data[15:0]] <= reg_x;
-            `INSTR_MEM_SY_DIRECT:
-                ram[instr_data[15:0]] <= reg_y;
-            `INSTR_ADD:
-                reg_accu <= reg_x + reg_y;
-            `INSTR_SUB:
-                reg_accu <= reg_x - reg_y;
-            `INSTR_MUL:
-                reg_accu <= reg_x * reg_y;
-            `INSTR_DIV:
-                reg_accu <= reg_x / reg_y;
+        address_bus <= address_bus;
+        reg_pc <= reg_pc;
+        $display ("current_state %d", current_state);	
+        case(current_state)
+            0:
+                // prepare reading instruction
+                address_bus <= reg_pc;
+            1: begin
+                    // fetch instruction 
+                    reg_instr <= memory_readout;
+                    $display ("reg_instr read from memory_readout %h", memory_readout);	
+                    address_bus <= reg_pc + 1;
+                    case(memory_readout) 
+                        `INSTR_STX_DIRECT: $display("decoded instruction INSTR_STX_DIRECT");
+                        `INSTR_STY_DIRECT: $display("decoded instruction INSTR_STY_DIRECT");
+                        `INSTR_MEM_LX_DIRECT: $display("decoded instruction INSTR_MEM_LX_DIRECT");
+                        `INSTR_MEM_LY_DIRECT: $display("decoded instruction INSTR_MEM_LY_DIRECT");
+                        `INSTR_MEM_LX_INDIRECT: $display("decoded instruction INSTR_MEM_LX_INDIRECT");
+                        `INSTR_MEM_LY_INDIRECT: $display("decoded instruction INSTR_MEM_LY_INDIRECT");
+                        `INSTR_MEM_SX_DIRECT: $display("decoded instruction INSTR_MEM_SX_DIRECT");
+                        `INSTR_MEM_SY_DIRECT: $display("decoded instruction INSTR_MEM_SY_DIRECT");
+                        `INSTR_MEM_SX_INDIRECT: $display("decoded instruction INSTR_MEM_SX_INDIRECT");
+                        `INSTR_MEM_SY_INDIRECT: $display("decoded instruction INSTR_MEM_SY_INDIRECT");
+                        `INSTR_ADD: $display("decoded instruction INSTR_ADD");
+                        `INSTR_SUB: $display("decoded instruction INSTR_SUB");
+                        `INSTR_MUL: $display("decoded instruction INSTR_MUL");
+                        `INSTR_DIV: $display("decoded instruction INSTR_DIV");
+                        `INSTR_MOVE_X_ACCU: $display("decoded instruction INSTR_MOVE_X_ACCU");
+                        `INSTR_MOVE_Y_ACCU: $display("decoded instruction INSTR_MOVE_Y_ACCU");
+                        `INSTR_MOVE_X_Y: $display("decoded instruction INSTR_MOVE_X_Y");
+                        `INSTR_MOVE_Y_X: $display("decoded instruction INSTR_MOVE_Y_X");
+                        `INSTR_MOVE_ACCU_X: $display("decoded instruction INSTR_MOVE_ACCU_X");
+                        `INSTR_MOVE_ACCU_Y: $display("decoded instruction INSTR_MOVE_ACCU_Y");
+                    endcase
+                end
+            2: begin
+                    // fetch instruction data
+                    reg_instr_data <= memory_readout;
+                    $display ("reg_instr_data read from memory_readout %h", memory_readout);	
+                    // if needed, prepare read for the instruction
+                    case(reg_instr)
+                        `INSTR_STX_DIRECT:
+                            reg_x <= memory_readout;
+                        `INSTR_STY_DIRECT:
+                            reg_y <= memory_readout;
+                        `INSTR_MEM_LX_DIRECT:begin
+                                $display ("preparing read from %h", memory_readout);	
+                                address_bus <= memory_readout;
+                            end
+                        `INSTR_MEM_LY_DIRECT:begin
+                                $display ("preparing read from %h", memory_readout);	
+                                address_bus <= memory_readout;
+                            end
+                        `INSTR_MEM_SX_DIRECT: 
+                            address_bus <= memory_readout;
+                        `INSTR_MEM_SY_DIRECT:
+                            address_bus <= memory_readout;
+                        default: 
+                            address_bus <= address_bus;
+                    endcase
+                end
+            3:
+                // execute instruction
+                case(reg_instr)
+                    `INSTR_STX_DIRECT:
+                        reg_x <= reg_instr_data;
+                    `INSTR_STY_DIRECT:
+                        reg_y <= reg_instr_data;
+                    `INSTR_MEM_LX_DIRECT: begin
+                        $display ("reading %h", memory_readout);	
+                        reg_x <= memory_readout;
+                    end
+                    `INSTR_MEM_LY_DIRECT:
+                        reg_y <= memory_readout;
+                    `INSTR_MEM_SX_DIRECT: begin
+                        memory_writeout <= reg_x;
+                        memory_write_enable <= 1;
+                        $display ("writing %h to %h", reg_x, address_bus);	
+                    end
+                    `INSTR_MEM_SY_DIRECT:begin
+                        memory_writeout <= reg_y;
+                        memory_write_enable <= 1;
+                    end
+                    `INSTR_ADD:
+                        reg_accu <= reg_x + reg_y;
+                    `INSTR_SUB:
+                        reg_accu <= reg_x - reg_y;
+                    `INSTR_MUL:
+                        reg_accu <= reg_x * reg_y;
+                    `INSTR_DIV:
+                        reg_accu <= reg_x / reg_y;
+                endcase
+            4:begin
+                // advance PC, disable memwrite
+                reg_pc <= reg_pc + 2;
+                memory_write_enable <= 0;
+            end
+            default:
+                reg_pc <= reg_pc;
         endcase
+
+        current_state <= (current_state + 1) % 5;
     end
 
     assign out_reg_x = reg_x;
     assign out_reg_y = reg_y;
     assign out_reg_accu = reg_accu[15:0];
     assign out_carry = reg_accu[16:15];
+    assign out_reg_pc = reg_pc;
+    assign out_reg_sp = reg_sp;
 
 endmodule
--- raw_verilog_stuff/minicpu/tb.v
+++ raw_verilog_stuff/minicpu/tb.v
@@ -1,78 +1,97 @@
 `include "cpu.v"
 module counter_tb();
+
 // Declare inputs as regs and outputs as wires
-    reg [7:0] instr;
-    reg [15:0] instr_data;
+    reg [15:0] ram [0:(16 * 1024)];
+
+    wire [15:0] memory_readout;
+
+    wire [15:0] memory_writeout;
+    wire memory_write_enable;
+    wire [15:0] address_bus;
 
     wire [15:0] out_reg_x;
     wire [15:0] out_reg_y;
     wire [15:0] out_reg_accu;
+    wire [15:0] out_reg_pc;
+    wire [15:0] out_reg_sp;
 
     reg run_signal;
 
+    (* keep = "true", syn_preserve = 1 *)
+    cpu i_cpu1(
+        .clk(run_signal),
+        .rst(1'b0), // no reset for now
+        .memory_readout(memory_readout),
+        .memory_writeout(memory_writeout),
+        .memory_write_enable(memory_write_enable),
+        .address_bus(address_bus),
+        .out_reg_x(out_reg_x),
+        .out_reg_y(out_reg_y),
+        .out_reg_accu(out_reg_accu),
+        .out_reg_pc(out_reg_pc),
+        .out_reg_sp(out_reg_sp)
+    );
+
+    assign memory_readout = ram[address_bus];
+
+    always @(negedge run_signal) begin
+        if(memory_write_enable) begin
+            $display ("REALLY in TB writing %h to %h", memory_writeout, address_bus);	
+            ram[address_bus] <= memory_writeout;
+        end
+    end
+
+
+  integer i = 0;
 // Initialize all variables
-initial begin        
+initial begin     
+    
+
    //$dumpfile("test.vcd");
-   $dumpvars(0,run_signal);
-   $dumpvars(1,instr);
+//    $dumpvars(0,run_signal);
+//    $dumpvars(1,instr);
 
 //   $display ("time\trun_signal\tinstr\tX\tY, ACCU");	
-  $monitor ("T%g\t CLK %b, INSTR %h, INSTR_DATA %h, X %h, Y %h, ACCU %h", 
-	  $time, run_signal, instr, instr_data, out_reg_x, out_reg_y, out_reg_accu);
+  $monitor ("T%g\t CLK %b, address_bus %h, memory_readout %h, X %h, Y %h, ACCU %h, PC %h, SP %h", 
+	  $time, run_signal, address_bus, memory_readout, out_reg_x, out_reg_y, out_reg_accu, out_reg_pc, out_reg_sp);
 
-//   $display ("time\t clk reset counter");	
-//   $monitor ("%g\t %b   %b   %b", 
-// 	  $time, clock, reset, counter_out);	
-  run_signal = 0;       // initial value of clock
-  instr = `INSTR_STX_DIRECT;
-  instr_data = 16'h6;
-  #5 run_signal = 1;
-  #5 run_signal = 0;
+// x = 0x6
+ ram[0] = `INSTR_STX_DIRECT;
+ ram[1] = 16'h6;
 
-  instr = `INSTR_STY_DIRECT;
-  instr_data = 16'h6;
-  #5 run_signal = 1;
-  #5 run_signal = 0;
+// y = 0x3
+ ram[2] = `INSTR_STY_DIRECT;
+ ram[3] = 16'h3;
 
-  instr = `INSTR_DIV;
-  instr_data = 16'h00;
-  #5 run_signal = 1;
-  #5 run_signal = 0;
+// accu = x/y = 2
+ ram[4] = `INSTR_DIV;
+ ram[5] = 16'h0;
 
-$display ("setting X to 0x32");	
+// x = 0x32
+ ram[6] = `INSTR_STX_DIRECT;
+ ram[7] = 16'h32;
 
-  instr = `INSTR_STX_DIRECT;
-  instr_data = 16'h32;
-  #5 run_signal = 1;
-  #5 run_signal = 0;
+// ram[0x0102] = x = 0x32
+ ram[8] = `INSTR_MEM_SX_DIRECT;
+ ram[9] = 16'h0102;
 
-$display ("storing X to 0x0102");	
+// x = 0x36
+ ram[10] = `INSTR_STX_DIRECT;
+ ram[11] = 16'h36;
 
-  instr = `INSTR_MEM_SX_DIRECT;
-  instr_data = 16'h0102;
-  #5 run_signal = 1;
-  #5 run_signal = 0;
+// ram[0x0103] = x = 0x36
+ ram[12] = `INSTR_MEM_SX_DIRECT;
+ ram[13] = 16'h0103;
 
-$display ("setting X to 0x36");	
+// x = ram[0x0102] = 0x32 
+ ram[14] = `INSTR_MEM_LX_DIRECT;
+ ram[15] = 16'h0102;
 
-  instr = `INSTR_STX_DIRECT;
-  instr_data = 16'h36;
-  #5 run_signal = 1;
-  #5 run_signal = 0;
-
-$display ("storing X to 0x0103");	
-
-  instr = `INSTR_MEM_SX_DIRECT;
-  instr_data = 16'h0103;
-  #5 run_signal = 1;
-  #5 run_signal = 0;
-
-$display ("loading X from 0x0102");	
-
-  instr = `INSTR_MEM_LX_DIRECT;
-  instr_data = 16'h0102;
-  #5 run_signal = 1;
-  #5 run_signal = 0;
+    for (i = 0; i < 16 * 4; i = i + 1) begin
+        #5 run_signal = 1;
+        #5 run_signal = 0;
+    end
 
   #50 $finish;      // Terminate simulation
 end
@@ -100,16 +119,5 @@ end
 //    end
 
 //    assign shifter_data_out = out_reg_x[transmit_shift];
-
-    (* keep = "true", syn_preserve = 1 *)
-    cpu i_cpu1(
-        .clk(run_signal),
-        .rst(1'b0), // no reset for now
-        .instr(instr),
-        .instr_data(instr_data),
-        .out_reg_x(out_reg_x),
-        .out_reg_y(out_reg_y),
-        .out_reg_accu(out_reg_accu)
-    );
 
 endmodule
